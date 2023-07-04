0b3977ae20321c9e052b91cf0dd9117e30ad5302
Oren Amsalem <oren.amsalem1@mail.huji.ac.il>
2023-07-04 12:11:41 -0400

    name changes for pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tissue-tag',
-    version='0.1.3',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=[
         'opencv-python',
