808dd00fcda8ffeeaac198d42d343a5e6cc11e29
Oren Amsalem <oren.amsalem1@mail.huji.ac.il>
2023-07-04 11:50:48 -0400

    add init needed for pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tissue-tag',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'opencv-python',
